a55bdef1919bd02e33a9a808d0e4d436c99e21b8
Daniel Miller & Spyder Monkee McHenry <student@epicodus.com>
2014-03-04 16:49:53 -0800

    nth prime number working

--- lib/prime_sift.rb
+++ lib/prime_sift.rb
@@ -12,11 +12,33 @@
 
 def prime_sift(number)
 
-
-
-  (2...number).to_a.each do |prime| 
-    self.reject!{ |num| num % prime == 0 && !(prime == num) }
-    
+numbers = Array(2...number)
+  numbers.each do |prime| 
+    numbers.reject!{ |num| num % prime == 0 && !(prime == num) }  
   end
 end
 
+def nth_prime(number)
+  primes = [2,3]
+  upper_number = 4
+  numbers = (2..upper_number).to_a
+  number_to_check = 0
+
+  while primes.length < number do
+    numbers.each do |num|
+      if upper_number % num == 0 && upper_number != num
+        number_to_check = num
+        break
+      else 
+        number_to_check = upper_number
+      end
+    end
+    if number_to_check == numbers[-1] 
+      primes.push(upper_number) 
+    end
+    upper_number += 1    
+    numbers = (2..upper_number).to_a
+  end
+
+  primes[-1]
+end
--- spec/prime_sift_spec.rb
+++ spec/prime_sift_spec.rb
@@ -3,8 +3,14 @@ require 'prime_sift'
 
 describe ('prime_sift') do
   it('creates an array with all prime numbers less than a given number') do
-    prime_sift(100).should(eq([2,3]))
+    prime_sift(5).should(eq([2,3]))
   end 
 end
+describe('nth_prime') do
+  it('returns the prime number at a given nth index') do
+    nth_prime(5).should(eq(11))
+  end
+end
+
 
 
